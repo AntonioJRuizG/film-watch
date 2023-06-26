c2970cfa19da7f3ce6bf8549a66c5d4fe757f939
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-26 19:31:13 +0200

    Style movie card

--- src/components/card/movie.card.jsx
+++ src/components/card/movie.card.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
+import styles from './movie.card.module.scss';
+
 export default function MovieCard({ movies }) {
 	return (
 		<>
 			{movies.map((movie) => (
-				<li key={movie.id}>
+				<li key={movie.id} className={styles.card}>
 					<h3>{movie.title}</h3>
 					<p>{movie.year}</p>
 					<img src={movie.image} alt={movie.title}></img>
