6113d313b89bfffef0185ff74fdabfe1056fb967
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-26 13:27:51 +0200

    Add header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import SearchForm from '../search.form/search.form';
+
+export default function Header() {
+	return (
+		<header>
+			<h1>Movies search engine</h1>
+			<SearchForm></SearchForm>
+		</header>
+	);
+}
