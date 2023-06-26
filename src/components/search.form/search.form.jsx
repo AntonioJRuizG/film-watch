621194648c5f95ec5285022c854f3f18265c3feb
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-26 15:11:19 +0200

    Make controlled form

--- src/components/search.form/search.form.jsx
+++ src/components/search.form/search.form.jsx
@@ -3,20 +3,25 @@ import styles from './search.form.module.scss';
 
 export default function SearchForm() {
 	const [searchValue, setSearchValue] = useState('');
+
 	const handleChange = (event) => {
 		const element = event.target;
 		setSearchValue(element.value);
 	};
 
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	};
+
 	return (
 		<>
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleSubmit}>
 				<input
 					type='text'
 					name='search'
 					value={searchValue}
 					placeholder='Forrest Gump, Jumanji, Avengers ...'
-					onChange={(event) => handleChange(event)}
+					onChange={handleChange}
 				></input>
 				<button type='submit'>Search</button>
 			</form>
