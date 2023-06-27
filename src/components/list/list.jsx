534752b58e8ebd479be3b9e25a3fa5ae0623978a
AntonioJesusRG <antoniogar129@gmail.com>
2023-06-27 02:54:02 +0200

    Improve list

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -4,19 +4,20 @@ import styles from './list.module.scss';
 import { MoviesContext } from '../../context/MoviesContext';
 
 export default function MoviesList() {
-	const { movies } = useContext(MoviesContext);
+	const { movies, loading } = useContext(MoviesContext);
 
 	const hasMovies = movies?.length > 0;
 
 	return (
 		<section>
-			{hasMovies ? (
-				<ul className={styles.listContainer}>
-					<MovieCard movies={movies}></MovieCard>
-				</ul>
-			) : (
-				<p>No results</p>
-			)}
+			{(loading && <p>Loading movies...</p>) ||
+				(hasMovies ? (
+					<ul className={styles.listContainer}>
+						<MovieCard movies={movies}></MovieCard>
+					</ul>
+				) : (
+					<p>No results</p>
+				))}
 		</section>
 	);
 }
